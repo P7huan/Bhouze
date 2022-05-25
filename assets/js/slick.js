f06e95dc4e05136cd3f7a48ded6e49b4a74fbaa2
P7huan <pth2707@gmail.com>
2022-05-25 17:32:29 +0700

    th

--- assets/js/slick.js
+++ assets/js/slick.js
@@ -12,7 +12,7 @@ $('.testimonial__slides').slick({
   slidesToShow: 1,
   slidesToScroll: 1,
   autoplay: 5000,
-  prevArrow: "<img class='a-left control-c prev slick-prev' src='./assets/images/arrow_prev.svg'>",
-  nextArrow: "<img class='a-right control-c next slick-next' src='./assets/images/arrow_next.svg'>",
+  prevArrow: "<img class='a-left control-c prev slick-prev testimonial__prev' src='./assets/images/arrow_prev.svg'>",
+  nextArrow: "<img class='a-right control-c next slick-next testimonial__prev' src='./assets/images/arrow_next.svg'>",
   dots: true,
 })
