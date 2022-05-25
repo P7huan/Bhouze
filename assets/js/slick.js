c57e074f3dab9d67fe0d35d4c66aedf162cd61b8
P7huan <pth2707@gmail.com>
2022-05-25 17:15:52 +0700

    th07

---
+++ assets/js/slick.js
@@ -0,0 +1,18 @@
+$('.slide__container').slick({
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: 5000,
+  dots: true,
+  prevArrow: "<img class='a-left control-c prev slick-prev' src='./assets/images/arrow_prev.svg'>",
+  nextArrow: "<img class='a-right control-c next slick-next' src='./assets/images/arrow_next.svg'>",
+})
+$('.testimonial__slides').slick({
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: 5000,
+  prevArrow: "<img class='a-left control-c prev slick-prev' src='./assets/images/arrow_prev.svg'>",
+  nextArrow: "<img class='a-right control-c next slick-next' src='./assets/images/arrow_next.svg'>",
+  dots: true,
+})
